b066a880b944b90740d4b3241f2e18a94cf6274d
Daniel Pfeifer <daniel@pfeifer-mail.de>
2010-04-23 16:08:36 +0200

    search for quickbook locally

--- AddDocumentation.cmake
+++ AddDocumentation.cmake
@@ -14,7 +14,6 @@ macro(add_documentation INPUT)
   endif(INPUT_PATH STREQUAL "")
 
   set(QBK_FILE ${CMAKE_CURRENT_BINARY_DIR}/${THIS_PROJECT_NAME}.qbk)
-  set(XML_FILE ${CMAKE_CURRENT_BINARY_DIR}/${THIS_PROJECT_NAME}.xml)
   set(DBK_FILE ${CMAKE_CURRENT_BINARY_DIR}/${THIS_PROJECT_NAME}.docbook)
 
   # copy to destination directory because quickbook screws up xinclude paths 
@@ -23,23 +22,14 @@ macro(add_documentation INPUT)
     COMMAND ${CMAKE_COMMAND} -E copy ${INPUT_PATH} ${QBK_FILE}
     DEPENDS ${INPUT_PATH})
 
-  add_custom_command(OUTPUT ${XML_FILE}
-    COMMAND quickbook "--output-file=${XML_FILE}" ${QBK_FILE}
-    DEPENDS ${QBK_FILE} ${ARGN})
-    
-  xsl_transform(${DBK_FILE} ${XML_FILE}
-    STYLESHEET ${BOOSTBOOK_XSL_DIR}/docbook.xsl
-    CATALOG ${BOOSTBOOK_CATALOG}
-    COMMENT "Generating DocBook from ${XML_FILE}."
-    DEPENDS ${XML_FILE}
-    )
+  quickbook_to_docbook(${DBK_FILE} ${QBK_FILE} ${ARGN})
 
   xsl_transform(${CMAKE_CURRENT_BINARY_DIR}/html ${DBK_FILE}
     STYLESHEET ${BOOSTBOOK_XSL_DIR}/html.xsl
     CATALOG ${BOOSTBOOK_CATALOG}
     DIRECTORY HTML.manifest
     COMMENT "Generating HTML documentaiton for ${THIS_PROJECT_NAME}."
-    MAKE_ALL_TARGET ${THIS_PROJECT_NAME}-html
+    MAKE_TARGET ${THIS_PROJECT_NAME}-html
     )
 
  xsl_transform(${CMAKE_CURRENT_BINARY_DIR}/man ${DBK_FILE}
@@ -47,6 +37,6 @@ macro(add_documentation INPUT)
    CATALOG ${BOOSTBOOK_CATALOG}
    DIRECTORY man.manifest
    COMMENT "Generating man pages for ${THIS_PROJECT_NAME}."
-   MAKE_ALL_TARGET ${THIS_PROJECT_NAME}-man)
+   MAKE_TARGET ${THIS_PROJECT_NAME}-man)
 
 endmacro(add_documentation INPUT)
--- FindBoostBook.cmake
+++ FindBoostBook.cmake
@@ -1,35 +1,33 @@
 
-#Find the BoostBook XSL stylesheets
-find_path(BOOSTBOOK_XSL_DIR docbook.xsl
-  PATHS /usr/share/boostbook/xsl/
-  DOC "Path to the BoostBook XSL stylesheets")
-if(NOT BOOSTBOOK_XSL_DIR)
-  message(SEND_ERROR "The BoostBook XSL stylesheets could not be found!")
-endif(NOT BOOSTBOOK_XSL_DIR)
+include(FindPackageHandleStandardArgs)
 
-#Find the BoostBook DTD
-find_path(BOOSTBOOK_DTD_DIR boostbook.dtd
-  PATHS /usr/share/boostbook/dtd/
-  DOC "Path to the BoostBook DTD")
-if(NOT BOOSTBOOK_DTD_DIR)
-  message(SEND_ERROR "The BoostBook DTD could not be found!")
-endif(NOT BOOSTBOOK_DTD_DIR)
+# Find the QuickBook executable
+find_program(QUICKBOOK_EXECUTABLE quickbook)
+find_package_handle_standard_args(QUICKBOOK DEFAULT_MSG QUICKBOOK_EXECUTABLE)
 
 # Find the DocBook DTD (version 4.2)
 find_path(DOCBOOK_DTD_DIR docbookx.dtd
   PATHS /usr/share/xml/docbook/schema/dtd/4.2
   DOC "Path to the DocBook DTD")
-if(NOT DOCBOOK_DTD_DIR)
-  message(SEND_ERROR "The DocBook DTD (version 4.2) could not be found!")
-endif(NOT DOCBOOK_DTD_DIR)
+find_package_handle_standard_args(DOCBOOK_DTD DEFAULT_MSG DOCBOOK_DTD_DIR)
 
 # Find the DocBook XSL stylesheets
 find_path(DOCBOOK_XSL_DIR html/html.xsl
   PATHS /usr/share/xml/docbook/stylesheet/docbook-xsl 
   DOC "Path to the DocBook XSL stylesheets")
-if(NOT DOCBOOK_XSL_DIR)
-  message(SEND_ERROR "The DocBook XSL stylesheets could not be found!")
-endif(NOT DOCBOOK_XSL_DIR)
+find_package_handle_standard_args(DOCBOOK_XSL DEFAULT_MSG DOCBOOK_XSL_DIR)
+
+#Find the BoostBook DTD
+find_path(BOOSTBOOK_DTD_DIR boostbook.dtd
+  PATHS /usr/share/boostbook/dtd/
+  DOC "Path to the BoostBook DTD")
+find_package_handle_standard_args(BOOSTBOOK_DTD DEFAULT_MSG BOOSTBOOK_DTD_DIR)
+
+#Find the BoostBook XSL stylesheets
+find_path(BOOSTBOOK_XSL_DIR docbook.xsl
+  PATHS /usr/share/boostbook/xsl/
+  DOC "Path to the BoostBook XSL stylesheets")
+find_package_handle_standard_args(BOOSTBOOK_XSL DEFAULT_MSG BOOSTBOOK_XSL_DIR)
 
 set(BOOSTBOOK_CATALOG ${CMAKE_BINARY_DIR}/boostbook_catalog.xml)
 file(WRITE ${BOOSTBOOK_CATALOG}
@@ -39,20 +37,56 @@ file(WRITE ${BOOSTBOOK_CATALOG}
   "  \"http://www.oasis-open.org/committees/entity/release/1.0/catalog.dtd\">\n"
   "<catalog xmlns=\"urn:oasis:names:tc:entity:xmlns:xml:catalog\">\n"
   "  <rewriteURI"
-    " uriStartString=\"http://www.boost.org/tools/boostbook/xsl/\""
-    " rewritePrefix=\"file://${BOOSTBOOK_XSL_DIR}/\""
-    "/>\n"
-  "  <rewriteURI"
-    " uriStartString=\"http://www.boost.org/tools/boostbook/dtd/\""
-    " rewritePrefix=\"file://${BOOSTBOOK_DTD_DIR}/\""
+    " uriStartString=\"http://www.oasis-open.org/docbook/xml/4.2/\""
+    " rewritePrefix=\"file://${DOCBOOK_DTD_DIR}/\""
     "/>\n"
   "  <rewriteURI"
     " uriStartString=\"http://docbook.sourceforge.net/release/xsl/current/\""
     " rewritePrefix=\"file://${DOCBOOK_XSL_DIR}/\""
     "/>\n"
   "  <rewriteURI"
-    " uriStartString=\"http://www.oasis-open.org/docbook/xml/4.2/\""
-    " rewritePrefix=\"file://${DOCBOOK_DTD_DIR}/\""
+    " uriStartString=\"http://www.boost.org/tools/boostbook/dtd/\""
+    " rewritePrefix=\"file://${BOOSTBOOK_DTD_DIR}/\""
+    "/>\n"
+  "  <rewriteURI"
+    " uriStartString=\"http://www.boost.org/tools/boostbook/xsl/\""
+    " rewritePrefix=\"file://${BOOSTBOOK_XSL_DIR}/\""
     "/>\n"
   "</catalog>\n"
   )
+
+# Transform Quickbook into BoostBook XML
+macro(quickbook_to_boostbook OUTPUT INPUT)
+
+  # If INPUT is not a full path, it's in the current source directory.
+  get_filename_component(INPUT_PATH ${INPUT} PATH)
+  if(INPUT_PATH STREQUAL "")
+    set(INPUT_PATH "${CMAKE_CURRENT_SOURCE_DIR}/${INPUT}")
+  else(INPUT_PATH STREQUAL "")
+    set(INPUT_PATH ${INPUT})
+  endif(INPUT_PATH STREQUAL "")
+
+  add_custom_command(OUTPUT ${OUTPUT}
+    COMMAND ${QUICKBOOK_EXECUTABLE} "--output-file=${OUTPUT}" ${INPUT_PATH}
+    COMMENT "Generating BoostBook XML from ${INPUT}."
+    DEPENDS ${INPUT} ${ARGN})
+
+endmacro(quickbook_to_boostbook OUTPUT INPUT)
+
+# Transform BoostBook XML into DocBook
+macro(boostbook_to_docbook OUTPUT INPUT)
+  xsl_transform(${OUTPUT} ${INPUT}
+    STYLESHEET ${BOOSTBOOK_XSL_DIR}/docbook.xsl
+    CATALOG ${BOOSTBOOK_CATALOG}
+    COMMENT "Generating DocBook from ${INPUT}."
+    DEPENDS ${INPUT} ${ARGN}
+    )
+endmacro(boostbook_to_docbook OUTPUT INPUT)
+
+# Transform Quickbook into BoostBook XML, then BoostBook XML into DocBook
+macro(quickbook_to_docbook OUTPUT INPUT)
+  get_filename_component(XML_FILE ${INPUT} NAME_WE)
+  set(XML_FILE ${XML_FILE}.xml)
+  quickbook_to_boostbook(${XML_FILE} ${INPUT})
+  boostbook_to_docbook(${OUTPUT} ${XML_FILE} ${ARGN})
+endmacro(quickbook_to_docbook OUTPUT INPUT)
